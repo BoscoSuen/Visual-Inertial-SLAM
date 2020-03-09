c653911c83999d15fb692a7476030a7de801aa83
Yukinichi <boscosuen@outlook.com>
2020-03-08 23:28:48 -0700

    got a result

--- EKF_process.py
+++ EKF_process.py
@@ -13,6 +13,6 @@ def predict(mu,sigma,delta_t,cur_linear_velocity,cur_rotation_velocity,W,N):
 	ut_curly_up_hat[3:6, 3:6] = up_hat(cur_rotation_velocity)
 	ut_curly_up_hat[0:3, 3:6] = up_hat(cur_linear_velocity)
 	temp = np.dot(sigma, np.transpose(expm(-delta_t * ut_curly_up_hat)))
-	sigma = np.dot(expm(-delta_t * ut_curly_up_hat), temp) + W
+	sigma = np.dot(expm(-delta_t * ut_curly_up_hat), temp) + delta_t ** 2 *W
 
 	return mu,sigma
